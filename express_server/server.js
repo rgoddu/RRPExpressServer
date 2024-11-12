ba4c0c5b690ab333f89d575f3efbf0cd1eb80b85
Remi G <rgoddu909@gmail.com>
2024-11-12 14:03:33 -0500

    re-ordering for readability

--- express_server/server.js
+++ express_server/server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.get('/*', (req, res) => {
     res.send('hello world')
 });
-
-const PORT = process.env.PORT || 3000;
 
 app.listen( PORT , '0.0.0.0', () => {
     console.log('server is listening on port', PORT)
